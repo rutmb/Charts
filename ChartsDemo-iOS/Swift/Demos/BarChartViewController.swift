7831f90ddc12e28ab457f55ca9cb974c3e7c436b
Igor Rudenko <tamboff69@gmail.com>
2022-02-15 14:19:30 +0300

    feat: added a corner radius settings to the BarCharts

--- ChartsDemo-iOS/Swift/Demos/BarChartViewController.swift
+++ ChartsDemo-iOS/Swift/Demos/BarChartViewController.swift
@@ -46,7 +46,12 @@ class BarChartViewController: DemoBaseViewController {
         chartView.drawValueAboveBarEnabled = false
         
         chartView.maxVisibleCount = 60
-        
+      
+        chartView.barSettings = .init(
+          rectCorner: [.allCorners],
+          cornerRadii: CGSize(width: 8, height: 8)
+        )
+          
         let xAxis = chartView.xAxis
         xAxis.labelPosition = .bottom
         xAxis.labelFont = .systemFont(ofSize: 10)
